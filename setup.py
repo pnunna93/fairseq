033b00f5efbd07dd89c59f9e40d24c4b3beb55ac
hakankiymaz-amd <hakan.kiymaz@amd.com>
2025-03-24 07:07:09 -0500

    update requirement

--- setup.py
+++ setup.py
@@ -214,7 +214,7 @@ def do_setup(package_data):
             "cffi",
             "cython",
             'dataclasses; python_version<"3.7"',
-            "hydra-core>=1.3.2",
+            "hydra-core==1.1.2",
             "omegaconf",
             'numpy<1.20.0; python_version<"3.7"',
             'numpy; python_version>="3.7"',
