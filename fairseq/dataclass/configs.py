1c6774953704c7c3125151f1242b3858ab17947d
hakankiymaz-amd <hakan.kiymaz@amd.com>
2025-03-24 04:21:23 -0500

    upd data class

--- fairseq/dataclass/configs.py
+++ fairseq/dataclass/configs.py
@@ -1042,4 +1042,4 @@ class FairseqConfig(FairseqDataclass):
     scoring: Any = None
     bpe: Any = None
     tokenizer: Any = None
-    ema: EMAConfig = EMAConfig(default=EMAConfig)
+    ema: EMAConfig = field(default=EMAConfig)
